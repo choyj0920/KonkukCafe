00b52cadb2aca9f99921fbe80e72da6899ea7bc1
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2023-05-04 13:57:18 +0900

    Initial commit

---
+++ app/src/main/java/com/kounkukcafe/kounkukcafe/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.kounkukcafe.kounkukcafe
+
+import android.content.Intent
+import android.media.FaceDetector.Face
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.kounkukcafe.kounkukcafe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val nextIntent = Intent(this, VoiceActivity::class.java)
+            startActivity(nextIntent)
+        }
+        binding.button2.setOnClickListener {
+            val nextIntent = Intent(this, FaceActivity::class.java)
+            startActivity(nextIntent)
+        }
+        binding.button3.setOnClickListener {
+            val nextIntent = Intent(this, CafeListActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/kounkukcafe/kounkukcafe/VoiceActivity.kt
@@ -0,0 +1,11 @@
+package com.kounkukcafe.kounkukcafe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_voice)
+    }
+}
