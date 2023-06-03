dd46451e2675333b85db4e989bfada3459216184
STARiv2r <williameom10@gmail.com>
2023-06-03 22:34:20 +0900

    Voice 종합 기능 완성본

--- app/src/main/java/com/kounkukcafe/kounkukcafe/VoiceRecordingActivity.kt
+++ app/src/main/java/com/kounkukcafe/kounkukcafe/VoiceRecordingActivity.kt
@@ -7,6 +7,8 @@ import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.kounkukcafe.kounkukcafe.apiutil.ApiManager
 import com.kounkukcafe.kounkukcafe.databinding.ActivityVoiceRecordingBinding
@@ -16,6 +18,7 @@ class VoiceRecordingActivity : AppCompatActivity() {
     lateinit var binding: ActivityVoiceRecordingBinding
     lateinit var mRecognizer: SpeechRecognizer
     lateinit var tts: TextToSpeech
+    var question_text = arrayOf("오늘 하루는 어떠셨나요?","휴일에 하고싶은 일이 있나요?","먹고싶은 음식이 있나요?" )
     var isTtsReady = false
     var isSttReady = false
     var emotion = ""
@@ -23,39 +26,63 @@ class VoiceRecordingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityVoiceRecordingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initTTs()
+
         init()
+        initTTs()
         initStt()
 
     }
+
     fun goNext(){
-        val nextIntent = Intent(this, CafeListActivity::class.java)
+
+        val nextIntent = Intent(this, VoiceLoadingActivity::class.java)
         nextIntent.putExtra("emotion",emotion.trim())
         startActivity(nextIntent)
+
     }
 
     fun initStt(){
+        Log.i("init Stt", "init stt")
         mRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
         mRecognizer.setRecognitionListener(listener)
+        isSttReady = true
         startStt()
 
     }
     fun startStt(){
+
         if(isSttReady){
+            Log.e("Stt", "stt ready2")
             intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
                 putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
                 putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
             }
+            Log.i("Stt", "stt ok")
             mRecognizer.startListening(intent)
         }
 
     }
     private fun init(){
-        binding.voiceIcon.setOnClickListener {
-            if (isTtsReady)
-                tts.speak(binding.askingTextView.text.toString(), TextToSpeech.QUEUE_ADD, null, null)
-        }
+        select_question()
 
+        binding.apply {
+            voiceIcon.setOnClickListener {
+                if (isTtsReady)
+                    tts.speak(
+                        binding.askingTextView.text.toString(),
+                        TextToSpeech.QUEUE_ADD,
+                        null,
+                        null
+                    )
+            }
+            questionRetryButton.setOnClickListener {
+                select_question()
+                voiceTextView.text = "인식 중"
+                questionRetryButton.visibility = View.GONE
+                startStt()
+            }
+
+        }
 
     }
 
@@ -67,11 +94,22 @@ class VoiceRecordingActivity : AppCompatActivity() {
                 tts.speak(binding.askingTextView.text.toString(), TextToSpeech.QUEUE_ADD, null, null)
             }
         }
+
+
+
+    }
+
+    private fun select_question(){
+        val random = (0..2).random()
+
+        binding.askingTextView.text = question_text[random]
+
     }
 
     override fun onStop() {
         super.onStop()
         tts.stop()
+
     }
 
     override fun onDestroy() {
@@ -106,7 +144,8 @@ class VoiceRecordingActivity : AppCompatActivity() {
                 SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "말하는 시간초과"
                 else -> "알 수 없는 오류임"
             }
-            Toast.makeText(applicationContext, "에러가 발생하였습니다. : $message", Toast.LENGTH_SHORT).show()
+            binding.voiceTextView.text = "인식 실패"
+            binding.questionRetryButton.visibility = View.VISIBLE
         }
 
         override fun onResults(results: Bundle?) {
@@ -122,6 +161,10 @@ class VoiceRecordingActivity : AppCompatActivity() {
             if(inputtext.length != 0){
                 ApiManager.callEmotionrecognitionText(inputtext,binding.voiceRecordedText)
             }
+
+            binding.voiceTextView.text = "인식 완료"
+            binding.questionRetryButton.visibility = View.VISIBLE
+            goNext()
         }
 
         override fun onPartialResults(partialResults: Bundle?) {}
